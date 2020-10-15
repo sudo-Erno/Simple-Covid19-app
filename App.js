3af2d3bcbee01efd3981ae229f39dc3d22b91a58
sudo-Erno <72893363+sudo-Erno@users.noreply.github.com>
2020-10-15 09:55:26 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -4,22 +4,12 @@ import './bootstrap/dist/css/bootstrap.css';
 import axios from 'axios';
 import Form from './Components/form';
 import './Components/styles.css';
-import Result from './Components/results';
 import Table from './Components/table';
+import options from './apiKey';
 
 function App() {
   const [covidStats, setCovidStats] = useState([]);
 
-  const options = {
-    method: 'GET',
-    url: 'https://rapidapi.p.rapidapi.com/v1/stats',
-    params: {country: ''},
-    headers: {
-      'x-rapidapi-host': 'covid-19-coronavirus-statistics.p.rapidapi.com',
-      'x-rapidapi-key': '226a9fbbd2msh3098f86f2468cdap1c076djsn8554e634c93c'
-    }
-  };
-  
   function capitalize(str){
     return str.slice(0, 1).toUpperCase() + str.slice(1, str.length);  
   }
